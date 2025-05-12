9590d8d19a741cc1ae81bd77da7ed74933f2163d
Gomina Aksinya <a.e.gomina@gmail.com>
2025-05-12 17:43:52 +0300

    Commit 2 Url-адреса конкатенироватys с базовым (BASE_URL)

--- urls.py
+++ urls.py
@@ -4,8 +4,8 @@
 class URL:
 
     BASE_URL = 'https://qa-scooter.praktikum-services.ru'
-    CREATE_COURIER = BASE_URL + '/api/v1/courier'
-    LOGIN_COURIER = BASE_URL + '/api/v1/courier/login'
-    DELETE_COURIER = BASE_URL + '/api/v1/courier/'
-    CREATE_ORDER = BASE_URL + '/api/v1/orders'
-    ORDER_LIST = BASE_URL + '/api/v1/orders'
+    CREATE_COURIER = 'https://qa-scooter.praktikum-services.ru/api/v1/courier'
+    LOGIN_COURIER = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/login'
+    DELETE_COURIER = 'https://qa-scooter.praktikum-services.ru/api/v1/courier/'
+    CREATE_ORDER = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
+    ORDER_LIST = 'https://qa-scooter.praktikum-services.ru/api/v1/orders'
